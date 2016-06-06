9bcfa5fc0d669c6ef217d8baae0f24e759ff3a4a
Chengzhi Jia <j.chengzhi1989@gmail.com>
2016-06-05 21:13:42 -0700

    binaryTreePaths

---
+++ src/LeetCode/Problem251To260.java
@@ -0,0 +1,53 @@
+package LeetCode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by ChengzhiJia on 6/5/16.
+ */
+public class Problem251To260 {
+
+    /*
+    257. Binary Tree Paths
+    Given a binary tree, return all root-to-leaf paths.
+
+For example, given the following binary tree:
+
+   1
+ /   \
+2     3
+ \
+  5
+All root-to-leaf paths are:
+
+["1->2->5", "1->3"]
+     */
+    public class TreeNode {
+        public int val;
+        public TreeNode left, right;
+        public TreeNode(int val) {
+            this.val = val;
+            this.left = this.right = null;
+        }
+    }
+
+    public List<String> binaryTreePaths(TreeNode root) {
+        ArrayList<String> result = new ArrayList<>();
+        if (root == null) return result;
+        pathSearch(result, "", root);
+        return result;
+    }
+
+    void pathSearch(ArrayList<String> result, String str, TreeNode node) {
+        if (!str.equals("")) str += "->";
+        str += node.val;
+        if (node.left == null && node.right == null) {
+            result.add(str);
+        } else {
+            if (node.left != null) pathSearch(result, str, node.left);
+            if (node.right != null) pathSearch(result, str, node.right);
+        }
+    }
+
+}
