b0ce386d2a5fe07679e5fbf5bb637661f3310104
Chengzhi Jia <j.chengzhi1989@gmail.com>
2016-07-13 23:09:56 -0700

    productExceptSelf

--- src/LeetCode/Problem231To240.java
+++ src/LeetCode/Problem231To240.java
@@ -21,6 +21,34 @@ Supposed the linked list is 1 -> 2 -> 3 -> 4 and you are given the third node wi
     }
 
     /*
+    238. Product of Array Except Self
+    Given an array of n integers where n > 1, nums, return an array output such that output[i] is equal to the product of all the elements of nums except nums[i].
+
+Solve it without division and in O(n).
+
+For example, given [1,2,3,4], return [24,12,8,6].
+
+Follow up:
+Could you solve it with constant space complexity? (Note: The output array does not count as extra space for the purpose of space complexity analysis.)
+     */
+
+    //Best Solution from LeetCode discussion
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] res = new int[n];
+        res[0] = 1;
+        for (int i = 1; i < n; i++) {
+            res[i] = res[i - 1] * nums[i - 1];
+        }
+        int right = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            res[i] *= right;
+            right *= nums[i];
+        }
+        return res;
+    }
+
+    /*
     240. Search a 2D Matrix II
     Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
 
