5f398e39d47966b691258e3820510dc82c12e292
Chengzhi Jia <j.chengzhi1989@gmail.com>
2016-07-14 00:08:13 -0700

    climbStairs

--- src/LeetCode/Problem61To70.java
+++ src/LeetCode/Problem61To70.java
@@ -35,4 +35,23 @@ The digits are stored such that the most significant digit is at the head of the
         } else return digits;
     }
 
+    /*
+    70. Climbing Stairs
+    You are climbing a stair case. It takes n steps to reach to the top.
+
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+     */
+    public int climbStairs(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+        int result[] = new int[n];
+        result[0] = 1;
+        result[1] = 2;
+        for (int i = 2; i < n; i++) {
+            result[i] = result[i - 1] + result[i - 2];
+        }
+        return result[n - 1];
+    }
+
 }
