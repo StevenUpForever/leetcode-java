f543470dbb545e7dd42f493820237bc9cc165e6e
Chengzhi Jia <j.chengzhi1989@gmail.com>
2016-07-21 22:05:36 -0700

    add 53

--- src/LeetCode/Problem51To60.java
+++ src/LeetCode/Problem51To60.java
@@ -8,6 +8,25 @@ import java.util.*;
 public class Problem51To60 {
 
     /*
+    53. Maximum Subarray
+    Find the contiguous subarray within an array (containing at least one number) which has the largest sum.
+
+For example, given the array [−2,1,−3,4,−1,2,1,−5,4],
+the contiguous subarray [4,−1,2,1] has the largest sum = 6.
+     */
+    public int maxSubArray(int[] nums) {
+        int result = 0;
+        if (nums.length == 0) return result;
+        for (int num: nums) {
+            result += num;
+            if (num >= 0) {
+                result = Math.max(result, num);
+            }
+        }
+        return result;
+    }
+
+    /*
     56. Merge Intervals
     Given a collection of intervals, merge all overlapping intervals.
 
