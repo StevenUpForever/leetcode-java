32792df1370367bc70254081907a64ec907d95fa
Chengzhi Jia <j.chengzhi1989@gmail.com>
2016-08-04 14:47:38 -0700

    288 changes

--- src/LeetCode/Problem281To290.java
+++ src/LeetCode/Problem281To290.java
@@ -35,25 +35,24 @@ isUnique("make") -> true
     public class ValidWordAbbr {
 
         HashMap<String, String> map = new HashMap<>();
+
         public ValidWordAbbr(String[] dictionary) {
             for(String str:dictionary){
-                String key = getKey(str);
-                if(map.containsKey(key)){
-                    if(!map.get(key).equals(str)){
-                        map.put(key, "");
-                    }
-                }
-                else{
+                String key = convertString(str);
+                if(map.containsKey(key) && !map.get(key).equals(str)) {
+                    map.put(key, "");
+                } else {
                     map.put(key, str);
                 }
             }
         }
 
         public boolean isUnique(String word) {
-            return !map.containsKey(getKey(word)) || map.get(getKey(word)).equals(word);
+            String key = convertString(word);
+            return !map.containsKey(key) || map.get(key).equals(word);
         }
 
-        String getKey(String str){
+        private String convertString(String str){
             if(str.length()<=2) return str;
             return str.charAt(0)+Integer.toString(str.length()-2)+str.charAt(str.length()-1);
         }
