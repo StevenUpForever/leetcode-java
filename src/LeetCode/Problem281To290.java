882ac095cd6fedc097ba1de4fffb577183e5ac68
Chengzhi Jia <j.chengzhi1989@gmail.com>
2017-02-05 01:12:00 -0800

    283

--- src/LeetCode/Problem281To290.java
+++ src/LeetCode/Problem281To290.java
@@ -6,6 +6,31 @@ import java.util.HashMap;
  * Created by stevenjia on 8/4/16.
  */
 public class Problem281To290 {
+
+    /*
+    283. Move Zeroes
+    Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+For example, given nums = [0, 1, 0, 3, 12], after calling your function, nums should be [1, 3, 12, 0, 0].
+
+Note:
+You must do this in-place without making a copy of the array.
+Minimize the total number of operations.
+     */
+    /*
+    2个隔板同向而行，快慢指针，不会改变原来数据的顺序
+     */
+    public void moveZeroes(int[] nums) {
+        if (nums == null || nums.length < 2) return;
+        int n = 0;
+        for (int i = 0; i < nums.length; ++i) {
+            if (nums[i] != 0) nums[n++] = nums[i];
+        }
+        for (int j = n; j < nums.length; ++j) {
+            nums[j] = 0;
+        }
+    }
+
     /*
     288. Unique Word Abbreviation
     An abbreviation of a word follows the form <first letter><number><last letter>. Below are some examples of word abbreviations:
