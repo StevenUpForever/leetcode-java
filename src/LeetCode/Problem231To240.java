def8129ab3e206753a00aebaec43d0559d9bdea6
Chengzhi Jia <c0j00cs@m-C02S82CLG8WM.local>
2017-03-15 17:19:08 -0700

    236

--- src/LeetCode/Problem231To240.java
+++ src/LeetCode/Problem231To240.java
@@ -43,13 +43,39 @@ For example, the lowest common ancestor (LCA) of nodes 2 and 8 is 6. Another exa
     /*
     Approach: Use feature about call stack in recursive method, which each node need its left and right subtree to notify if any p or q found, if so, return itself to its super node, if no nodes found, return null, until the end when root node's call stack finished
      */
+//    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+//        if (root == null || root == p || root == q) return root; //Base case of recursion steps, find any node or reach bottom and found no node
+//        TreeNode left = lowestCommonAncestor(root.left, p, q);
+//        TreeNode right = lowestCommonAncestor(root.right, p, q);
+//        //Do call stack steps about left subtree search result and right substree search result
+//        if (left != null && right != null) return root;
+//        else if (left == null && right == null) return null;
+//        else return left == null ? right : left;
+//    }
+
+    /*
+    236. Lowest Common Ancestor of a Binary Tree
+    Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the tree.
+
+According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes v and w as the lowest node in T that has both v and w as descendants (where we allow a node to be a descendant of itself).”
+
+        _______3______
+       /              \
+    ___5__          ___1__
+   /      \        /      \
+   6      _2       0       8
+         /  \
+         7   4
+For example, the lowest common ancestor (LCA) of nodes 5 and 1 is 3. Another example is LCA of nodes 5 and 4 is 5, since a node can be a descendant of itself according to the LCA definition.
+     */
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if (root == null || root == p || root == q) return root; //Base case of recursion steps, find any node or reach bottom and found no node
+        if (root == null || root == p || root == q) {
+            return root;
+        }
         TreeNode left = lowestCommonAncestor(root.left, p, q);
         TreeNode right = lowestCommonAncestor(root.right, p, q);
-        //Do call stack steps about left subtree search result and right substree search result
-        if (left != null && right != null) return root; 
-        else if (left == null && right == null) return null;
+        if (left == null && right == null) return null;
+        else if (left != null && right != null) return root;
         else return left == null ? right : left;
     }
 
