72dd197fbc466bfa5b0ee718f2b65ebf42f80bda
Chengzhi Jia <j.chengzhi1989@gmail.com>
2017-03-15 23:07:40 -0700

    117

--- src/LeetCode/Problem111To120.java
+++ src/LeetCode/Problem111To120.java
@@ -41,16 +41,63 @@ After calling your function, the tree should look like:
         TreeLinkNode next;
     }
 
+//    public void connect(TreeLinkNode root) {
+//         if (root == null) return;
+//         while (root.left != null) {
+//             TreeLinkNode cur = root;
+//             while (cur != null) {
+//                 cur.left.next = cur.right;
+//                 if (cur.next != null) cur.right.next = cur.next.left;
+//                 cur = cur.next;
+//             }
+//             root = root.left;
+//         }
+//    }
+
+    /*
+    117. Populating Next Right Pointers in Each Node II
+    Follow up for problem "Populating Next Right Pointers in Each Node".
+
+What if the given tree could be any binary tree? Would your previous solution still work?
+
+Note:
+
+You may only use constant extra space.
+For example,
+Given the following binary tree,
+         1
+       /  \
+      2    3
+     / \    \
+    4   5    7
+After calling your function, the tree should look like:
+         1 -> NULL
+       /  \
+      2 -> 3 -> NULL
+     / \    \
+    4-> 5 -> 7 -> NULL
+     */
     public void connect(TreeLinkNode root) {
-         if (root == null) return;
-         while (root.left != null) {
+         while (root != null) {
              TreeLinkNode cur = root;
+             TreeLinkNode temp = null;
+             TreeLinkNode nextHead = null;
              while (cur != null) {
-                 cur.left.next = cur.right;
-                 if (cur.next != null) cur.right.next = cur.next.left;
+                 if (cur.left != null) {
+                     if (temp != null) temp.next = cur.left;
+                     else nextHead = cur.left;
+                     temp = cur.left;
+                 }
+                 if (cur.right != null) {
+                     if (temp != null) temp.next = cur.right;
+                     else nextHead = cur.right;
+                     temp = cur.right;
+                 }
                  cur = cur.next;
              }
-             root = root.left;
+             root = nextHead;
+             temp = null;
+             nextHead = null;
          }
     }
 }
