d571316bdbc1fa24d8a5af689a8bbafc9c956917
Chengzhi Jia <j.chengzhi1989@gmail.com>
2017-03-30 02:03:40 -0700

    91

--- src/LeetCode/Problem91To100.java
+++ src/LeetCode/Problem91To100.java
@@ -5,6 +5,42 @@ package LeetCode;
  */
 public class Problem91To100 {
     /*
+    91. Decode Ways
+    A message containing letters from A-Z is being encoded to numbers using the following mapping:
+
+'A' -> 1
+'B' -> 2
+...
+'Z' -> 26
+Given an encoded message containing digits, determine the total number of ways to decode it.
+
+For example,
+Given encoded message "12", it could be decoded as "AB" (1 2) or "L" (12).
+
+The number of ways decoding "12" is 2.
+
+Show Company Tags
+Show Tags
+     */
+    /*
+    Approach: DP problem, M[0] = 0, M[i] represent the max decode ways for substring to i
+    M[i] = max(M[0...j]) + 1 if subString j...i could be encoded, 0 <= j < i
+    111
+     */
+    public int numDecodings(String s) {
+        if (s == null || s.length() == 0) return 0;
+        char[] chars = s.toCharArray();
+        int len = chars.length;
+        int[] dp = new int[len + 1];
+        dp[len]  = 1;
+        dp[len - 1] = chars[len - 1] != '0' ? 1 : 0;
+        for (int i = len - 2; i >= 0; i--) {
+            if (chars[i] != '0') dp[i] = (chars[i] - '0') * 10 + chars[i + 1] - '0' <= 26 ? dp[i + 1] + dp[i + 2] : dp[i + 1];
+        }
+        return dp[0];
+    }
+
+    /*
     98. Validate Binary Search Tree
     Given a binary tree, determine if it is a valid binary search tree (BST).
 
--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,13 @@
+import LeetCode.Problem91To100;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-//        Main newMain = new Main();
-//        ArrayList<Integer> arr = new ArrayList<>();
-//        arr.add(5);
-//        arr.add(10);
-//        arr.add(2);
-//        arr.add(1);
-//        ArrayList<Integer> B = newMain.performOps(arr);
-//        for (int i = 0; i < B.size(); i++) {
-//            System.out.print(B.get(i) + " ");
-//        }
-        System.out.print("abc".substring(0, "abc".length()));
-    }
-
-    ArrayList<Integer> performOps(ArrayList<Integer> A) {
-        ArrayList<Integer> B = new ArrayList<Integer>();
-        for (int i = 0; i < 2 * A.size(); i++) B.add(0);
-        for (int i = 0; i < A.size(); i++) {
-            B.set(i, A.get(i));
-            B.set(i + A.size(), A.get((A.size() - i) % A.size()));
-        }
-        return B;
+        Problem91To100 problem = new Problem91To100();
+        System.out.println(problem.numDecodings("101"));
     }
 
 
