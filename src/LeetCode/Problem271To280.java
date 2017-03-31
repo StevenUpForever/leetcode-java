b727bc86700b0239ac294ec16e933e9325bb8bcd
Chengzhi Jia <j.chengzhi1989@gmail.com>
2017-03-30 17:20:16 -0700

    278

--- src/LeetCode/Problem271To280.java
+++ src/LeetCode/Problem271To280.java
@@ -108,4 +108,23 @@ private String helper(int num) {
         if (resLen > 0 && res.charAt(resLen - 1) == ' ') res.deleteCharAt(resLen - 1);
         return res.toString();
     }
+
+    /*
+    278. First Bad Version
+    You are a product manager and currently leading a team to develop a new product. Unfortunately, the latest version of your product fails the quality check. Since each version is developed based on the previous version, all the versions after a bad version are also bad.
+
+Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
+
+You are given an API bool isBadVersion(version) which will return whether version is bad. Implement a function to find the first bad version. You should minimize the number of calls to the API.
+     */
+//    public int firstBadVersion(int n) {
+//        if (n < 1) return 0;
+//        int start = 1;
+//        while (start < n - 1) {
+//            int mid = start + (n - start)/2;
+//            if (isBadVersion(mid)) n = mid;
+//            else start = mid;
+//        }
+//        return isBadVersion(start) ? start : n;
+//    }
 }
