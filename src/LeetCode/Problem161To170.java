e8c507e2c36e0f8567ac0aa9b7c305bad7c4a05c
Chengzhi Jia <j.chengzhi1989@gmail.com>
2017-03-31 20:56:44 -0700

    161

--- src/LeetCode/Problem161To170.java
+++ src/LeetCode/Problem161To170.java
@@ -6,6 +6,23 @@ package LeetCode;
 public class Problem161To170 {
 
     /*
+    161. One Edit Distance
+    Given two strings S and T, determine if they are both one edit distance apart.
+     */
+    public boolean isOneEditDistance(String s, String t) {
+        int len1 = s.length(), len2 = t.length();
+        if (Math.abs(len1 - len2) > 1) return false;
+        for (int i = 0; i < Math.min(len1, len2); i++) {
+            if (s.charAt(i) != t.charAt(i)) {
+                if (len1 == len2) return s.substring(i + 1).equals(t.substring(i + 1));
+                else if (len1 < len2) return s.substring(i).equals(t.substring(i + 1));
+                else return s.substring(i + 1).equals(t.substring(i));
+            }
+        }
+        return Math.abs(len1 - len2) == 1;
+    }
+
+    /*
     162. Find Peak Element
     A peak element is an element that is greater than its neighbors.
 
