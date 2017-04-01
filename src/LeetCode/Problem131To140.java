46f214039ba3deb8ef18f046b95f701b8651c116
Chengzhi Jia <j.chengzhi1989@gmail.com>
2017-03-31 22:07:37 -0700

    133

--- src/LeetCode/Problem131To140.java
+++ src/LeetCode/Problem131To140.java
@@ -9,17 +9,70 @@ import java.util.*;
  */
 public class Problem131To140 {
 
-    private class RandomListNode {
+    /*
+    133. Clone Graph
+    Clone an undirected graph. Each node in the graph contains a label and a list of its neighbors.
+
+
+OJ's undirected graph serialization:
+Nodes are labeled uniquely.
+
+We use # as a separator for each node, and , as a separator for node label and each neighbor of the node.
+As an example, consider the serialized graph {0,1,2#1,2#2,2}.
+
+The graph has a total of three nodes, and therefore contains three parts as separated by #.
+
+First node is labeled as 0. Connect node 0 to both nodes 1 and 2.
+Second node is labeled as 1. Connect node 1 to node 2.
+Third node is labeled as 2. Connect node 2 to node 2 (itself), thus forming a self-cycle.
+Visually, the graph looks like the following:
+
+       1
+      / \
+     /   \
+    0 --- 2
+         / \
+         \_/
+     */
+
+    /*
+    Approach: key point is keep track of already visited original node and copy node
+    so use HashMap to save original node label as key and copy node as value and return node
+     */
+    class UndirectedGraphNode {
         int label;
-        RandomListNode next, random;
-        RandomListNode(int x) { this.label = x; }
-    };
+        List<UndirectedGraphNode> neighbors;
+        UndirectedGraphNode(int x) { label = x; neighbors = new ArrayList<UndirectedGraphNode>(); }
+    }
+
+    public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
+        if (node == null) return null;
+        HashMap<Integer, UndirectedGraphNode> map = new HashMap<>();
+        return cloneGraphHelper(node, map);
+    }
+
+    private UndirectedGraphNode cloneGraphHelper(UndirectedGraphNode node, HashMap<Integer, UndirectedGraphNode> map) {
+        if (node == null) return null;
+        if (map.containsKey(node.label)) return map.get(node.label);
+        UndirectedGraphNode clone = new UndirectedGraphNode(node.label);
+        map.put(clone.label, clone);
+        for (UndirectedGraphNode neighbor: node.neighbors) {
+            clone.neighbors.add(cloneGraphHelper(neighbor, map));
+        }
+        return clone;
+    }
     /*
     138. Copy List with Random Pointer
     A linked list is given such that each node contains an additional random pointer which could point to any node in the list or null.
 
 Return a deep copy of the list.
      */
+    private class RandomListNode {
+        int label;
+        RandomListNode next, random;
+        RandomListNode(int x) { this.label = x; }
+    }
+
     public RandomListNode copyRandomList(RandomListNode head) {
         HashMap<RandomListNode, RandomListNode> map = new HashMap<RandomListNode, RandomListNode>();
         RandomListNode temp = head;
