13d2e0680c5fb6a672edff660479f6f606feacd0
Chengzhi Jia <j.chengzhi1989@gmail.com>
2017-03-31 21:25:56 -0700

    121

--- src/LeetCode/Problem121To130.java
+++ src/LeetCode/Problem121To130.java
@@ -6,6 +6,34 @@ package LeetCode;
 public class Problem121To130 {
 
     /*
+    121. Best Time to Buy and Sell Stock
+    Say you have an array for which the ith element is the price of a given stock on day i.
+
+If you were only permitted to complete at most one transaction (ie, buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+
+Example 1:
+Input: [7, 1, 5, 3, 6, 4]
+Output: 5
+
+max. difference = 6-1 = 5 (not 7-1 = 6, as selling price needs to be larger than buying price)
+Example 2:
+Input: [7, 6, 4, 3, 1]
+Output: 0
+
+In this case, no transaction is done, i.e. max profit = 0.
+     */
+    public int maxProfit(int[] prices) {
+        int res = 0;
+        if (prices.length == 0) return res;
+        int min = prices[0];
+        for (int i = 1; i < prices.length; i++) {
+            res = Math.max(prices[i] - min, res);
+            min = Math.min(prices[i], min);
+        }
+        return res;
+    }
+
+    /*
  125. Valid Palindrome
  Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
 
