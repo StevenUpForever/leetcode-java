069a73f761d8b9d7b386fc75f9ebb07a4a27725c
Chengzhi Jia <j.chengzhi1989@gmail.com>
2017-04-27 11:51:14 -0700

    4Sum

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import LeetCode.Problem271To280;
 import LeetCode.Problem91To100;
+import Problem11To20.FourSum;
 import Problem1To10.Median_Two_Sorted_Arrays;
 import Problem1To10.String_to_Integer;
 
@@ -9,8 +10,8 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        String_to_Integer newObj = new String_to_Integer();
-        System.out.print(newObj.myAtoi("1"));
+        FourSum obj = new FourSum();
+        System.out.print(obj.fourSum(new int[]{1,0,-1,0,-2,2}, 0));
     }
 
 }
