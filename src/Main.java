89b21bcc9f55ad4eb384d6e22686dcc1199fd20f
Chengzhi Jia <j.chengzhi1989@gmail.com>
2017-05-04 12:47:29 -0700

    two another way solutions about 30

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ import Problem1To10.Median_Two_Sorted_Arrays;
 import Problem1To10.String_to_Integer;
 import Problem21To30.Divide_Two_Integers;
 import Problem21To30.Generate_Parentheses;
+import Problem21To30.Substring_with_Concatenation_of_All_Words;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -12,6 +13,8 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        Substring_with_Concatenation_of_All_Words obj = new Substring_with_Concatenation_of_All_Words();
+        System.out.print(obj.findSubstring1("lingmindrabofooowingdingbarrwingmonkeypoundcake", new String[]{"fooo","barr","wing","ding","wing"}));
     }
 
 }
