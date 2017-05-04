b37e7a9ce78da80a177c6a26c214926617292ff4
Chengzhi Jia <j.chengzhi1989@gmail.com>
2017-05-03 17:47:13 -0700

    29. Divide Two Integers

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import LeetCode.Problem91To100;
 import Problem11To20.FourSum;
 import Problem1To10.Median_Two_Sorted_Arrays;
 import Problem1To10.String_to_Integer;
+import Problem21To30.Divide_Two_Integers;
 import Problem21To30.Generate_Parentheses;
 
 import java.util.ArrayList;
@@ -11,8 +12,6 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Generate_Parentheses obj = new Generate_Parentheses();
-        System.out.print(obj.generateParenthesis(3));
     }
 
 }
