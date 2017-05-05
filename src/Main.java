672a8ae5d15486d71ffdf20d4de97390505ab479
Chengzhi Jia <j.chengzhi1989@gmail.com>
2017-05-04 18:00:30 -0700

    31. Next Permutation

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ import Problem1To10.String_to_Integer;
 import Problem21To30.Divide_Two_Integers;
 import Problem21To30.Generate_Parentheses;
 import Problem21To30.Substring_with_Concatenation_of_All_Words;
+import Problem31To40.Next_Permutation;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -13,8 +14,10 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Substring_with_Concatenation_of_All_Words obj = new Substring_with_Concatenation_of_All_Words();
-        System.out.print(obj.findSubstring1("wordgoodgoodgoodbestword", new String[]{"word","good","best","good"}));
+        Next_Permutation obj = new Next_Permutation();
+        int[] input = new int[]{1, 5, 1};
+        obj.nextPermutation(input);
+        for (int num: input) System.out.println(num);
     }
 
 }
