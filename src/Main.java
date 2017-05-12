98e8829a92d3984a5cea878146393f061f468401
Chengzhi Jia <j.chengzhi1989@gmail.com>
2017-05-12 11:21:00 -0700

    40. Combination Sum II

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ import Problem1To10.String_to_Integer;
 import Problem21To30.Divide_Two_Integers;
 import Problem21To30.Generate_Parentheses;
 import Problem21To30.Substring_with_Concatenation_of_All_Words;
+import Problem31To40.Combination_Sum_II;
 import Problem31To40.Count_and_Say;
 import Problem31To40.Longest_Valid_Parentheses;
 import Problem31To40.Next_Permutation;
@@ -16,8 +17,8 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Count_and_Say obj = new Count_and_Say();
-        System.out.println(obj.countAndSay(4));
+        Combination_Sum_II obj = new Combination_Sum_II();
+        System.out.println(obj.combinationSum2(new int[]{10,1,2,7,6,1,5}, 8));
     }
 
 }
