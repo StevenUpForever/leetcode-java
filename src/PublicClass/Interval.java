bb2b735cb18efc57e07e970307de62ba21573ee6
Chengzhi Jia <j.chengzhi1989@gmail.com>
2017-05-17 19:38:45 -0700

    56. Merge Intervals

---
+++ src/PublicClass/Interval.java
@@ -0,0 +1,8 @@
+package PublicClass;
+
+public class Interval {
+    public int start;
+    public int end;
+    Interval() { start = 0; end = 0; }
+    Interval(int s, int e) { start = s; end = e; }
+}
