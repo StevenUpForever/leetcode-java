01a2e2a28e390c9c79421f12d15ba4982a113d00
Chengzhi Jia <j.chengzhi1989@gmail.com>
2017-05-20 16:47:37 -0700

    70. Climbing Stairs

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,7 @@
-import LeetCode.Problem271To280;
-import LeetCode.Problem91To100;
-import Problem11To20.FourSum;
-import Problem1To10.Median_Two_Sorted_Arrays;
-import Problem1To10.String_to_Integer;
-import Problem21To30.Divide_Two_Integers;
-import Problem21To30.Generate_Parentheses;
-import Problem21To30.Substring_with_Concatenation_of_All_Words;
-import Problem31To40.Combination_Sum_II;
-import Problem31To40.Count_and_Say;
-import Problem31To40.Longest_Valid_Parentheses;
-import Problem31To40.Next_Permutation;
-import Problem41To50.First_Missing_Positive;
-import Problem41To50.Jump_Game_II;
-import Problem41To50.Multiply_Strings;
-import Problem41To50.Permutations;
-import Problem51To60.N_Queens;
-import Problem51To60.Permutation_Sequence;
-import Problem61To70.*;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Sqrt_x obj = new Sqrt_x();
-        System.out.print(obj.mySqrtS2(2147395599));
     }
 }
 
