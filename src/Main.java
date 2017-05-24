2a8459699c6590149809a263915fa0443af3f9f5
c0j00cs <j.chengzhi1989@gmail.com>
2017-05-24 13:34:38 -0700

    81. Search in Rotated Sorted Array II

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
+import Problem81To90.Search_in_Rotated_Sorted_Array_II;
 
 public class Main {
     public static void main(String[] args) {
-        
+        Search_in_Rotated_Sorted_Array_II obj = new Search_in_Rotated_Sorted_Array_II();
+        System.out.print(obj.search(new int[]{5,1,3}, 3));
     }
 }
 
