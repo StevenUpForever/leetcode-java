7076465e68e91d509a2a8256105db0983aa53727
Chengzhi Jia <j.chengzhi1989@gmail.com>
2017-05-26 00:07:46 -0700

    82. Remove Duplicates from Sorted List II

--- src/Problem81To90/Rm_Dup_from_Sorted_List_II.java
+++ src/Problem81To90/Rm_Dup_from_Sorted_List_II.java
@@ -15,12 +15,31 @@ public class Rm_Dup_from_Sorted_List_II {
 
     /**
      * Solution:
+     * 1. In a linkedList, if we want to skip the dups, we need to keep track of the previous node before the dups, so we need to have a pre node before dups, and a cur node to go the loop
+     * 2. Due we may need to skip first several nodes (dups from 1st node), as the same process in step 1, it's better to have a dummy node before head node, so
+     *      We need a dummy node insert before head, and return dummy.next as result
+     *      Need a pre node start from dummy, a cur node start from head
+     * A. for the while loop, first cur node skip all dups, at the end, cur is the last node of dups
+     * B. check if pre is still connected with cur, if so, cur has no dups, pre = cur, cur = cur.next
+     *      else cur has dups, pre.next = cur.next, cur = cur.next
+     * C. return dummy.next
      *
+     * Time: O(n)
+     * Space: O(1)
      */
 
     public ListNode deleteDuplicates(ListNode head) {
         ListNode dummy = new ListNode(0);
+        dummy.next = head;
         ListNode cur = head, pre = dummy;
+        while (cur != null) {
+            while (cur.next != null && cur.val == cur.next.val) cur = cur.next;
+            //At this time cur is the last node of dups or the index of that no dup node
+            if (pre.next != cur) pre.next = cur.next;
+            else pre = cur;
+            cur = cur.next;
+        }
+        return dummy.next;
     }
 
 }
