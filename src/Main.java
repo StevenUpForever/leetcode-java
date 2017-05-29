53d93ecc742c4e3c5550dea555ed92d71366cf96
Chengzhi Jia <j.chengzhi1989@gmail.com>
2017-05-29 01:45:04 -0700

    88. Merge Sorted Array

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
+import Problem81To90.Merge_Sorted_Array;
 import Problem81To90.Search_in_Rotated_Sorted_Array_II;
 
 public class Main {
     public static void main(String[] args) {
-        Search_in_Rotated_Sorted_Array_II obj = new Search_in_Rotated_Sorted_Array_II();
-        System.out.print(obj.search(new int[]{5,1,3}, 3));
+        Merge_Sorted_Array obj = new Merge_Sorted_Array();
+        int[] nums1 = new int[]{1, 3, 5, 0, 0, 0}, nums2 = new int[]{2, 4, 6};
+        obj.mergeS2(nums1, 3, nums2, 3);
+        for (int num: nums1) System.out.println(num);
     }
 }
 
