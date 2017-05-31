6f9896f8643d0308ebdcec754a548c19b8a99624
c0j00cs <j.chengzhi1989@gmail.com>
2017-05-31 16:44:11 -0700

    93. Restore IP Addresses

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,11 @@
 import Problem81To90.Merge_Sorted_Array;
 import Problem81To90.Search_in_Rotated_Sorted_Array_II;
+import Problem91To100.Restore_IP_Addresses;
 
 public class Main {
     public static void main(String[] args) {
-        Merge_Sorted_Array obj = new Merge_Sorted_Array();
-        int[] nums1 = new int[]{1, 3, 5, 0, 0, 0}, nums2 = new int[]{2, 4, 6};
-        obj.mergeS2(nums1, 3, nums2, 3);
-        for (int num: nums1) System.out.println(num);
+        Restore_IP_Addresses obj = new Restore_IP_Addresses();
+        System.out.print(obj.restoreIpAddresses("0000"));
     }
 }
 
