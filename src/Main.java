180fa1d66a3058859b3b06a8e0696a70dbebe11b
Chengzhi Jia <j.chengzhi1989@gmail.com>
2017-06-14 22:00:59 -0700

    187. Repeated DNA Sequences

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,6 @@ import Problem91To100.Restore_IP_Addresses;
 
 public class Main {
     public static void main(String[] args) {
-        Restore_IP_Addresses obj = new Restore_IP_Addresses();
-        System.out.print(obj.restoreIpAddresses("0000"));
     }
 }
 
