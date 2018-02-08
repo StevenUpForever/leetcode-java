9855e9b9b318981f0d13adef68c95ad899d3238b
Chengzhi Jia <j.chengzhi1989@gmail.com>
2018-02-07 18:17:46 -0800

    49. Group Anagrams

---
+++ src/others/GroupAnagrams.java
@@ -0,0 +1,55 @@
+package others;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+public class GroupAnagrams {
+
+    //TAG: Uber
+    //TAG: Others
+
+    /**
+     * 49. Group Anagrams
+     * Given an array of strings, group anagrams together.
+
+     For example, given: ["eat", "tea", "tan", "ate", "nat", "bat"],
+     Return:
+
+     [
+     ["ate", "eat","tea"],
+     ["nat","tan"],
+     ["bat"]
+     ]
+     Note: All inputs will be in lower-case.
+     */
+
+    /**
+     * Solution:
+     * Anagram problem, if there's one anagram, or all compare to one, could use HashMap or int[26/256] depends on String
+     * is ASCII only or unicode
+     * This problem need to check O^2 time by hashmap/int[] method, so:
+     * sort every string, then put in a map<String, List<String>>
+     *
+     * Time: O(n * nlog(n)) = O(n^2log(n))
+     * Space: O(n) if every string has no anagram
+     */
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, List<String>> map = new HashMap<>();
+        for (String str: strs) {
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+            String newStr = String.valueOf(chars);
+            if (!map.containsKey(newStr)) {
+                List<String> list = new ArrayList<>();
+                list.add(str);
+                map.put(newStr, list);
+            } else {
+                map.get(newStr).add(str);
+            }
+        }
+        return new ArrayList<>(map.values());
+    }
+
+}
