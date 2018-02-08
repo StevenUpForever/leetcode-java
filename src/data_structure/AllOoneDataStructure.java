adbe404747c0afee9cb06517246d8b1d5d9e3723
Chengzhi Jia <j.chengzhi1989@gmail.com>
2018-02-07 18:06:23 -0800

    432. All O`one Data Structure

--- src/data_structure/AllOoneDataStructure.java
+++ src/data_structure/AllOoneDataStructure.java
@@ -48,39 +48,94 @@ public class AllOoneDataStructure {
             map = new HashMap<>();
         }
 
+        /*
+        insert new node, it does following things:
+        if the string is new:
+            if the count 1 node is there, add key to node1 sets
+            if the count 1 node is not there, alloc a new node in front of tail and add key to set
+        if the string is in map, have count, increase count, remove it from the set:
+            if increase count node is not there, alloc
+            if there, add to increase count node's set
+         */
         /** Inserts a new key <Key> with value 1. Or increments an existing key by 1. */
         public void inc(String key) {
-            //If there's no node with key
+            //key is new
             if (!map.containsKey(key)) {
-                doubleListNode newNode = new doubleListNode(1);
-                newNode.set.add(key);
-                insertNewNode(newNode);
+                //if count 1 node is there
+                if (tail.pre.count != 1) {
+                    doubleListNode oneNode = new doubleListNode(1);
+                    insertNodeInFront(oneNode, tail);
+                }
+                tail.pre.set.add(key);
+                map.put(key, tail.pre);
             } else {
-                //There's existed node
+                doubleListNode cur = map.get(key);
+                cur.set.remove(key);
+                //First check if cur.pre.count = cur.count + 1, if not, alloc new node
+                if (cur.pre.count != cur.count + 1) {
+                    doubleListNode newNode = new doubleListNode(cur.count + 1);
+                    insertNodeInFront(newNode, cur);
+                }
+                cur.pre.set.add(key);
+                map.put(key, cur.pre);
+                //Remove node if cur.set is empty, so that when it's the max/min node, should not return any string
+                if (cur.set.isEmpty()) deleteNode(cur);
             }
         }
 
         /** Decrements an existing key by 1. If Key's value is 1, remove it from the data structure. */
         public void dec(String key) {
-
+            //Only do when key existed
+            if (map.containsKey(key)) {
+                doubleListNode node = map.get(key);
+                node.set.remove(key);
+                if (node.count == 1) {
+                    map.remove(key);
+                } else {
+                    if (node.next.count != node.count - 1) {
+                        doubleListNode newNode = new doubleListNode(node.count - 1);
+                        insertNodeBehind(newNode, node);
+                    }
+                    node.next.set.add(key);
+                    map.put(key, node.next);
+                }
+                if (node.set.isEmpty()) deleteNode(node);
+            }
         }
 
         /** Returns one of the keys with maximal value. */
         public String getMaxKey() {
-
+            //Check if head.next is not tail, if so, due to node.set is not null, return any string
+            return head.next.count != Integer.MIN_VALUE ? head.next.set.iterator().next() : "";
         }
 
         /** Returns one of the keys with Minimal value. */
         public String getMinKey() {
-
+            //Check if tail.pre is not tail, if so, due to node.set is not null, return any string
+            return tail.pre.count != Integer.MAX_VALUE ? tail.pre.set.iterator().next() : "";
         }
 
-        /*
-        insert new node, it does following things:
-        if the count 1 node is there,
-         */
-        private void insertNewNode(String key, int count) {
+        //Used when node.set is empty
+        private void deleteNode(doubleListNode node) {
+            node.pre.next = node.next;
+            node.next.pre = node.pre;
+        }
 
+        //Insert newnode in front of target
+        private void insertNodeInFront(doubleListNode newNode, doubleListNode target) {
+            doubleListNode pre = target.pre;
+            pre.next = newNode;
+            newNode.pre = pre;
+            newNode.next = target;
+            target.pre = newNode;
+        }
+
+        private void insertNodeBehind(doubleListNode newNode, doubleListNode target) {
+            doubleListNode next = target.next;
+            target.next = newNode;
+            newNode.pre = target;
+            newNode.next = next;
+            next.pre = newNode;
         }
 
     }
