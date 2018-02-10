e5e902ab4861fa819a0f209e2744008a6722430b
Chengzhi Jia <j.chengzhi1989@gmail.com>
2018-02-09 23:42:19 -0800

    230. Kth Smallest Element in a BST

--- src/KthSmallestElementInABST.java
+++ src/KthSmallestElementInABST.java
@@ -39,4 +39,24 @@ public class KthSmallestElementInABST {
         return 1 + countNodes(n.left) + countNodes(n.right);
     }
 
+    //S2
+    private static int number = 0;
+    private static int count = 0;
+
+    public int kthSmallest2(TreeNode root, int k) {
+        count = k;
+        helper(root);
+        return number;
+    }
+
+    public void helper(TreeNode n) {
+        if (n.left != null) helper(n.left);
+        count--;
+        if (count == 0) {
+            number = n.val;
+            return;
+        }
+        if (n.right != null) helper(n.right);
+    }
+
 }
