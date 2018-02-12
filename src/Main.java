c3ce5c886bf937bc3f4963621a2ccdf5219d8df5
Chengzhi Jia <j.chengzhi1989@gmail.com>
2018-02-11 18:40:55 -0800

    516. Longest Palindromic Subsequence

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,8 @@ import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) {
-        ValidPalindrome obj = new ValidPalindrome();
-        System.out.println(obj.isPalindrome("aa"));
-        Stack a = new Stack();
-        System.out.println('9' - '0');
+        LongestPalindromicSubsequence obj = new LongestPalindromicSubsequence();
+        System.out.println(obj.longestPalindromeSubseq("bbbab"));
     }
 }
 
