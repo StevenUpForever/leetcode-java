1e9cd3c0548b6a046b2a3c60a32cdb346aef1fb8
Chengzhi Jia <j.chengzhi1989@gmail.com>
2018-02-17 23:56:44 -0800

    345. Reverse Vowels of a String

---
+++ src/ReverseVowelsOfAString.java
@@ -0,0 +1,30 @@
+public class ReverseVowelsOfAString {
+
+    //TAG: Google
+    //Difficulty: Easy
+
+    /**
+     * 345. Reverse Vowels of a String
+     * Write a function that takes a string as input and reverse only the vowels of a string.
+
+     Example 1:
+     Given s = "hello", return "holle".
+
+     Example 2:
+     Given s = "leetcode", return "leotcede".
+
+     Note:
+     The vowels does not include the letter "y".
+     */
+
+    /**
+     * Solution:
+     *
+     */
+
+    public String reverseVowels(String s) {
+
+    }
+
+}
+
