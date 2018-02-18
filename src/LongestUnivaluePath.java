05e423c48995a5a4d9bb4cbf2b6f31f5ddcc17dd
Chengzhi Jia <j.chengzhi1989@gmail.com>
2018-02-18 12:09:52 -0800

    687. Longest Univalue Path

--- src/LongestUnivaluePath.java
+++ src/LongestUnivaluePath.java
@@ -3,6 +3,8 @@ import public_class.TreeNode;
 public class LongestUnivaluePath {
 
     //TAG: Google
+    //TAG: Tree
+    //TAG: bottom up
     //Difficulty: Easy
 
     /**
@@ -38,8 +40,33 @@ public class LongestUnivaluePath {
      Note: The given binary tree has not more than 10000 nodes. The height of the tree is not more than 1000.
      */
 
-    public int longestUnivaluePath(TreeNode root) {
+    /**
+     * Solution:
+     * DFS Tree bottom up
+     * pass current root.val to next levels, so that when track back, could compare current node with parent root val
+     *
+     * update global max every time, since non same value path will return 0
+     * if any sub node value is not equal to root, then return 0, start current longest path from beginning
+     * else return max(left, right) + 1
+     *
+     * Time: O(n)
+     * Space: (1)
+     */
 
+    public int longestUnivaluePath(TreeNode root) {
+        if (root == null) return 0;
+        int[] max = new int[]{0};
+        longestUnivaluePathHelper(root, root.val, max);
+        return max[0];
+    }
+
+    private int longestUnivaluePathHelper(TreeNode root, int val, int[] max) {
+        if (root == null) return 0;
+        int left = longestUnivaluePathHelper(root.left, root.val, max);
+        int right = longestUnivaluePathHelper(root.right, root.val, max);
+        max[0] = Math.max(max[0], left + right);
+        if (val == root.val)  return Math.max(left, right) + 1;
+        return 0;
     }
 
 }
