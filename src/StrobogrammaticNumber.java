35a1e51bac183c30f5c2bbc7edbdfd77de177785
Chengzhi Jia <j.chengzhi1989@gmail.com>
2018-02-17 23:57:57 -0800

    246. Strobogrammatic Number

---
+++ src/StrobogrammaticNumber.java
@@ -0,0 +1,24 @@
+public class StrobogrammaticNumber {
+
+    //TAG: Google
+    //Difficulty: Easy
+
+    /**
+     * 246. Strobogrammatic Number
+     * A strobogrammatic number is a number that looks the same when rotated 180 degrees (looked at upside down).
+
+     Write a function to determine if a number is strobogrammatic. The number is represented as a string.
+
+     For example, the numbers "69", "88", and "818" are all strobogrammatic.
+     */
+
+    /**
+     * Solution:
+     *
+     */
+
+    public boolean isStrobogrammatic(String num) {
+
+    }
+
+}
