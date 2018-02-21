afe4cdabbd9f8b733bf23418ccefab630d23b34e
Chengzhi Jia <j.chengzhi1989@gmail.com>
2018-02-20 23:43:41 -0800

    187. Repeated DNA Sequences

--- src/legacy_code/Problem181To190/Repeated_DNA_Sequences.java
+++ src/legacy_code/Problem181To190/Repeated_DNA_Sequences.java
@@ -7,6 +7,10 @@ import java.util.List;
 
 public class Repeated_DNA_Sequences {
 
+    //TAG: LinkedIn
+    //TAG: map_set
+    //Difficulty: Medium
+
     /**
      * 187. Repeated DNA Sequences
      * All DNA is composed of a series of nucleotides abbreviated as A, C, G, and T, for example: "ACGAATTCCG". When studying DNA, it is sometimes useful to identify repeated sequences within the DNA.
