fbc3f3a19f7ca286291e8a2be170f5b16b94759b
Chengzhi Jia <j.chengzhi1989@gmail.com>
2018-04-14 16:48:04 -0700

    158. Read N Characters Given Read4 II - Call multiple times

---
+++ src/data_structure/stream/ReadNCharsRead4II.java
@@ -0,0 +1,50 @@
+package data_structure.stream;
+
+public class ReadNCharsRead4II {
+
+    //TAG: Google
+    //TAG: Facebook
+    //TAG: Data structure
+    //TAG: Stream
+
+    /**
+     * 158. Read N Characters Given Read4 II - Call multiple times
+     * The API: int read4(char *buf) reads 4 characters at a time from a file.
+     *
+     * The return value is the actual number of characters read. For example, it returns 3 if there is only 3 characters left in the file.
+     *
+     * By using the read4 API, implement the function int read(char *buf, int n) that reads n characters from the file.
+     *
+     * Note:
+     * The read function may be called multiple times.
+     */
+
+    /**
+     * Solution:
+     * The key point to read char[] multiple times, is keep the visited index where current index at, after previous
+     * read, so that when run next read, could have preserved index where to start
+     */
+
+    private int index = 0;
+    private int count = 0;
+    private char[] curBuff = new char[4];
+    public int read(char[] buf, int n) {
+        int ptr = 0;
+        while (ptr < n) {
+            //If index == 0 means all chars already read from temp char[] to result char[] and need read next4 to index
+            if (index == 0) count = read4(curBuff);
+            //Use condition that min(chars actual read, n), read from temp buff to res buff
+            while (ptr < n && index < count) {
+                buf[ptr++] = curBuff[index++];
+            }
+            // *** this means if n < 4, temp buff is not fully read, need wait next read to read to res buff ***
+            if (index >= count) index = 0;
+        }
+        return ptr;
+    }
+
+    int read4(char[] buf) {
+        return 0;
+    }
+
+}
