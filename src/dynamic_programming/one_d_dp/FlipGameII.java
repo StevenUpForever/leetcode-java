2de721972755555fc4f60b61a183a9f5ec90dcfc
Chengzhi Jia <j.chengzhi1989@gmail.com>
2018-04-16 21:59:08 -0700

    294. Flip Game II

--- src/dynamic_programming/one_d_dp/FlipGameII.java
+++ src/dynamic_programming/one_d_dp/FlipGameII.java
@@ -4,6 +4,10 @@ import java.util.*;
 
 public class FlipGameII {
 
+    //TAG: Google
+    //TAG: DP
+    //Difficulty: Medium
+
     /**
      * 294. Flip Game II
      * You are playing the following Flip Game with your friend: Given a string that contains only these two characters: + and -, you and your friend take turns to flip two consecutive "++" into "--". The game ends when a person can no longer make a move and therefore the other person will be the winner.
