f269a79ca58a401445c0f595772a304e52a999c3
Chengzhi Jia <j.chengzhi1989@gmail.com>
2018-04-18 13:40:25 -0700

    UnionFind

--- src/depth_first_search/NumberOfIslands.java
+++ src/depth_first_search/NumberOfIslands.java
@@ -12,7 +12,9 @@ public class NumberOfIslands {
 
     /**
      * 200. Number of Islands
-     * Given a 2d grid map of '1's (land) and '0's (water), count the number of islands. An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+     * Given a 2d grid map of '1's (land) and '0's (water), count the number of islands.
+     * An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically.
+     * You may assume all four edges of the grid are all surrounded by water.
 
      Example 1:
 
@@ -32,7 +34,7 @@ public class NumberOfIslands {
      */
 
     /**
-     * Solution:
+     * Solution 1:
      * DFS, when find a '1', try to set all adjacent '1' to '0' (including itself)
      *
      * Then when met another 1, it must be another island
---
+++ src/public_class/UnionFind.java
@@ -0,0 +1,86 @@
+package public_class;
+
+public class UnionFind {
+
+    /**
+     * count is the union units found in current graph
+     *
+     * parent is the head of each union units which represent of current union unit
+     *
+     * rank is the count of current union units, which means when two units going to union,
+     * the rank is the rules which one merge to which one
+     */
+    private int count;
+    private int[] parents;
+    private int[] rank;
+
+    //For question like 200, a matrix initially with 1s
+    public UnionFind(char[][] grid) {
+        int row = grid.length;
+        int col = grid[0].length;
+        count = 0;
+        parents = new int[row * col];
+        rank = new int[row * col];
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1') {
+                    //Set parents array without consider about if islands could union
+                    parents[i * col + j] = 1;
+                    count++;
+                }
+                rank[i * col + j] = 0;
+            }
+        }
+    }
+
+    //For questions like 305, init with empty board and dynamically add numbers
+    public UnionFind(int n) {
+        count = 0;
+        parents = new int[n];
+        rank = new int[n];
+        for (int i = 0; i < n; i++) {
+            //Due to number may be 0
+            parents[i] = -1;
+            rank[i] = 0;
+        }
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    //Check wether current unit contains elements
+    public boolean isValid(int i) {
+        return parents[i] >= 0;
+    }
+
+    //When update parent, update the count too, fix count in union method
+    public void setParents(int i) {
+        parents[i] = i;
+        count++;
+    }
+
+    public int find(int i) {
+        /*
+        If parent of current is not itself, find the highest number of this unit, and set to current
+        to make the unit consistent
+         */
+        if (parents[i] != i) parents[i] = find(parents[i]);
+        return parents[i];
+    }
+
+    //union units contains i and j
+    public void union(int i, int j) {
+        //First find parent of these two units
+        int parent1 = find(i);
+        int parent2 = find(j);
+        //Compare rank of them, always merge lower rank one to higher rank
+        if (rank[parent1] < rank[parent2]) parents[parent1] = parent2;
+        else if (rank[parent1] > rank[parent2]) parents[parent2] = parent1;
+        else {
+            parents[parent2] = parent1;
+            rank[parent1]++;
+        }
+    }
+
+}
