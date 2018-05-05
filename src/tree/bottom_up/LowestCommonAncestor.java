a9e5eaf91ed8af5c2108c6f4371cebb9429a8b19
Chengzhi Jia <j.chengzhi1989@gmail.com>
2018-05-05 11:49:39 -0700

    236. Lowest Common Ancestor of a Binary Tree

--- src/tree/bottom_up/LowestCommonAncestor.java
+++ src/tree/bottom_up/LowestCommonAncestor.java
@@ -4,7 +4,11 @@ import public_class.TreeNode;
 
 public class LowestCommonAncestor {
 
+    //TAG: Facebook
+    //TAG: Microsoft
+    //TAG: Amazon
     //TAG: LinkedIn
+    //TAG: Apple
     //TAG: Tree
     //TAG: Bottom up
     //Difficulty: Medium
@@ -15,17 +19,17 @@ public class LowestCommonAncestor {
 
      According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes v and w as the lowest node in T that has both v and w as descendants (where we allow a node to be a descendant of itself).”
 
-     _______3______
-     /              \
-     ___5__          ___1__
+          _______3______
+         /              \
+      ___5__          ___1__
      /      \        /      \
      6      _2       0       8
-     /  \
-     7   4
+           /  \
+          7   4
      For example, the lowest common ancestor (LCA) of nodes 5 and 1 is 3. Another example is LCA of nodes 5 and 4 is 5, since a node can be a descendant of itself according to the LCA definition.
      */
 
-    /**
+    /*
      * Solution:
      * Classic back track problem
      * What to know at this node: the LCA from left node and right node
