c03ed78df1defd69a453ee4a719041feccb547d5
Chengzhi Jia <j.chengzhi1989@gmail.com>
2018-05-05 11:47:36 -0700

    20. Valid Parentheses

--- src/stack/ValidParentheses.java
+++ src/stack/ValidParentheses.java
@@ -14,11 +14,36 @@ public class ValidParentheses {
     /**
      * 20. Valid Parentheses
      * Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
-
-     The brackets must close in the correct order, "()" and "()[]{}" are all valid but "(]" and "([)]" are not.
+     *
+     * An input string is valid if:
+     *
+     * Open brackets must be closed by the same type of brackets.
+     * Open brackets must be closed in the correct order.
+     * Note that an empty string is also considered valid.
+     *
+     * Example 1:
+     *
+     * Input: "()"
+     * Output: true
+     * Example 2:
+     *
+     * Input: "()[]{}"
+     * Output: true
+     * Example 3:
+     *
+     * Input: "(]"
+     * Output: false
+     * Example 4:
+     *
+     * Input: "([)]"
+     * Output: false
+     * Example 5:
+     *
+     * Input: "{[]}"
+     * Output: true
      */
 
-    /**
+    /*
      * Solution:
      * Use stack to save the parenthesis
      * 1. When met left side parenthesis, push into stack
