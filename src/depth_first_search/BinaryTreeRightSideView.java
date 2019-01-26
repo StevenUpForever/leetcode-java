976163e6aaefbf0372ce677302815e3dc8989d2a
StevenUpForever <j.chengzhi1989@gmail.com>
2019-01-26 11:37:51 -0800

    199. Binary Tree Right Side View

---
+++ src/depth_first_search/BinaryTreeRightSideView.java
@@ -0,0 +1,53 @@
+package depth_first_search;
+
+import public_class.TreeNode;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class BinaryTreeRightSideView {
+
+    /**
+     * 199. Binary Tree Right Side View
+     *
+     * Given a binary tree, imagine yourself standing on the right side of it, return the values of the nodes you can see ordered from top to bottom.
+     *
+     * Example:
+     *
+     * Input: [1,2,3,null,5,null,4]
+     * Output: [1, 3, 4]
+     * Explanation:
+     *
+     *    1            <---
+     *  /   \
+     * 2     3         <---
+     *  \     \
+     *   5     4       <---
+     */
+
+    /*
+    Solution:
+    Use BFS or DFS, from right -> left, only add first occurrence number on current level
+
+    Time: O(n)
+    Space: O(n)
+     */
+
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        rightSideViewHelper(list, 0, root);
+        return list;
+    }
+
+    private void rightSideViewHelper(List<Integer> list, int index, TreeNode root) {
+        if (root == null) return;
+        if (index == list.size()) {
+            list.add(root.val);
+        }
+        rightSideViewHelper(list, index + 1, root.right);
+        rightSideViewHelper(list, index + 1, root.left);
+    }
+
+}
