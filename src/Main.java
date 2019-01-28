d88a79136b8955e37261776f8ab42b6bc8cbf426
StevenUpForever <j.chengzhi1989@gmail.com>
2019-01-27 18:18:11 -0800

    785. Is Graph Bipartite?

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,16 @@
-import string.integer_string_convert.BasicCalculatorII;
+import breadth_first_search.IsGraphBipartite;
 
 public class Main {
     public static void main(String[] args) {
 
-        BasicCalculatorII calculatorII = new BasicCalculatorII();
-        System.out.println(calculatorII.calculate("3+2*2"));
+        IsGraphBipartite obj = new IsGraphBipartite();
+        System.out.println(obj.isBipartite(new int[][]{{1,2,3},{0,3,4},{0,3}, {0,1,2}, {1}}));
+        System.out.println(obj.isBipartite(new int[][]{{3},{2, 4},{1}, {0,4}, {1,3}}));
+//        Set<Integer> set1 = new HashSet<>(), set2 = new HashSet<>();
+//        addToSets(new int[]{1, 2, 3}, set1, set2);
+//        for (int num: set1) System.out.println(set1);
     }
+
 
 
 }
---
+++ src/breadth_first_search/IsGraphBipartite.java
@@ -0,0 +1,117 @@
+package breadth_first_search;
+
+import java.nio.file.Path;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Set;
+
+public class IsGraphBipartite {
+
+    //Difficulty: medium
+    //TAG: Apple
+    //TAG: DFS
+    //TAG: BFS
+
+    /**
+     * 785. Is Graph Bipartite?
+     * Given an undirected graph, return true if and only if it is bipartite.
+     *
+     * Recall that a graph is bipartite if we can split it's set of nodes into two independent subsets A and B such that every edge in the graph has one node in A and another node in B.
+     *
+     * The graph is given in the following form: graph[i] is a list of indexes j for which the edge between nodes i and j exists.  Each node is an integer between 0 and graph.length - 1.  There are no self edges or parallel edges: graph[i] does not contain i, and it doesn't contain any element twice.
+     *
+     * Example 1:
+     * Input: [[1,3], [0,2], [1,3], [0,2]]
+     * Output: true
+     * Explanation:
+     * The graph looks like this:
+     * 0----1
+     * |    |
+     * |    |
+     * 3----2
+     * We can divide the vertices into two groups: {0, 2} and {1, 3}.
+     * Example 2:
+     * Input: [[1,2,3], [0,2], [0,1,3], [0,2]]
+     * Output: false
+     * Explanation:
+     * The graph looks like this:
+     * 0----1
+     * | \  |
+     * |  \ |
+     * 3----2
+     * We cannot find a way to divide the set of nodes into two independent subsets.
+     *
+     *
+     * Note:
+     *
+     * graph will have length in range [1, 100].
+     * graph[i] will contain integers in range [0, graph.length - 1].
+     * graph[i] will not contain i or duplicate values.
+     * The graph is undirected: if any element j is in graph[i], then i will be in graph[j].
+     */
+
+    /*
+    Solution:
+    The question actually is: e.g. if graph[i][j] = 1, then 1 is connected to all numbers in graph[1]
+     */
+
+    /*
+    Solution 1:
+    DFS
+     */
+
+    public boolean isBipartite(int[][] graph) {
+        int n = graph.length;
+        int[] colors = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            if (colors[i] == 0 && !validColor(graph, colors, 1, i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean validColor(int[][] graph, int[] colors, int color, int node) {
+        if (colors[node] != 0) {
+            return colors[node] == color;
+        }
+        colors[node] = color;
+        for (int next : graph[node]) {
+            if (!validColor(graph, colors, -color, next)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /*
+    Solution 2:
+    BFS
+     */
+
+    public boolean isBipartite2(int[][] graph) {
+        int[] visited = new int[graph.length];
+        for (int i = 0; i < graph.length; i++) {
+            if (graph[i].length != 0 && visited[i] == 0) {
+                visited[i] = 1;
+                Queue<Integer> q = new LinkedList<>();
+                q.offer(i);
+                while(! q.isEmpty()) {
+                    int current = q.poll();
+                    for (int c: graph[current]) {
+                        if (visited[c] == 0) {
+                            visited[c] = (visited[current] == 1) ? 2 : 1;
+                            q.offer(c);
+                        } else {
+                            if (visited[c] == visited[current]) return false;
+                        }
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+}
