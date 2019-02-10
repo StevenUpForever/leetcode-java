53010b6d2726f088fdd1aaaba0e0ecd7beb8ed4b
StevenUpForever <j.chengzhi1989@gmail.com>
2019-02-09 16:34:04 -0800

    380. Insert Delete GetRandom O(1)

--- src/data_structure/InsertDeleteGetRandomO1.java
+++ src/data_structure/InsertDeleteGetRandomO1.java
@@ -13,37 +13,43 @@ public class InsertDeleteGetRandomO1 {
 
     Design a data structure that supports all following operations in average O(1) time.
 
-insert(val): Inserts an item val to the set if not already present.
-remove(val): Removes an item val from the set if present.
-getRandom: Returns a random element from current set of elements. Each element must have the same probability of being returned.
-Example:
+    insert(val): Inserts an item val to the set if not already present.
+    remove(val): Removes an item val from the set if present.
+    getRandom: Returns a random element from current set of elements. Each element must have the same probability of being returned.
+    Example:
 
-// Init an empty set.
-RandomizedSet randomSet = new RandomizedSet();
+    // Init an empty set.
+    RandomizedSet randomSet = new RandomizedSet();
 
-// Inserts 1 to the set. Returns true as 1 was inserted successfully.
-randomSet.insert(1);
+    // Inserts 1 to the set. Returns true as 1 was inserted successfully.
+    randomSet.insert(1);
 
-// Returns false as 2 does not exist in the set.
-randomSet.remove(2);
+    // Returns false as 2 does not exist in the set.
+    randomSet.remove(2);
 
-// Inserts 2 to the set, returns true. Set now contains [1,2].
-randomSet.insert(2);
+    // Inserts 2 to the set, returns true. Set now contains [1,2].
+    randomSet.insert(2);
 
-// getRandom should return either 1 or 2 randomly.
-randomSet.getRandom();
+    // getRandom should return either 1 or 2 randomly.
+    randomSet.getRandom();
 
-// Removes 1 from the set, returns true. Set now contains [2].
-randomSet.remove(1);
+    // Removes 1 from the set, returns true. Set now contains [2].
+    randomSet.remove(1);
 
-// 2 was already in the set, so return false.
-randomSet.insert(2);
+    // 2 was already in the set, so return false.
+    randomSet.insert(2);
 
-// Since 2 is the only number in the set, getRandom always return 2.
-randomSet.getRandom();
+    // Since 2 is the only number in the set, getRandom always return 2.
+    randomSet.getRandom();
+
+     * Your RandomizedSet object will be instantiated and called as such:
+     * RandomizedSet obj = new RandomizedSet();
+     * boolean param_1 = obj.insert(val);
+     * boolean param_2 = obj.remove(val);
+     * int param_3 = obj.getRandom();
      */
 
-    /**
+    /*
     Solutions:
     Due to O(1) insert and remove, cannot use Array, OR WE CAN ADD AND REMOVE ELM AT LAST INDEX, WHICH RANDOM WILL BE O(1)
     Due to O(1) getRandom, cannot use LinkedList, or at least cannot only use LinkedList
@@ -98,15 +104,7 @@ randomSet.getRandom();
         }
     }
 
-/**
- * Your RandomizedSet object will be instantiated and called as such:
- * RandomizedSet obj = new RandomizedSet();
- * boolean param_1 = obj.insert(val);
- * boolean param_2 = obj.remove(val);
- * int param_3 = obj.getRandom();
- */
-
- /**
+ /*
  Follow up, what if the array could contains duplicate numbers
 
   Change HashMap to <Integer, LinkedList> when insert, always return true, and append index after list
