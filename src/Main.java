ed17636920743bdd326eee94560cb482efdfe973
StevenUpForever <j.chengzhi1989@gmail.com>
2019-02-09 20:09:31 -0800

    989. Add to Array-Form of Integer

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 import binary_search.Q34FindFirstAndLastPositionOfElementInSortedArray;
 
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
 
 
-        Q34FindFirstAndLastPositionOfElementInSortedArray obj = new Q34FindFirstAndLastPositionOfElementInSortedArray();
-        int[] res = obj.searchRange(new int[]{5,7,7,8,8,10}, 8);
-        System.out.println(res[0] + " " + res[1]);
+
 
     }
 
