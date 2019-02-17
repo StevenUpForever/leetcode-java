56e6739edf2ecc9369e5639ccdbd4bd63a0ecfc5
StevenUpForever <j.chengzhi1989@gmail.com>
2019-02-16 20:25:01 -0800

    993. Cousins in Binary Tree

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,159 @@
-import binary_search.Q69SqrtX;
-import string.sliding_window.Q30SubstringWithConcatenationOfAllWords;
+import public_class.TreeNode;
 
-import java.util.List;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Q69SqrtX obj = new Q69SqrtX();
-        System.out.println(obj.mySqrt(2147395599));
+        Main main = new Main();
+//        System.out.println(main.numSquarefulPerms(new int[]{13949,64411,26920,5204,2177,23617,44128,3455,47315,40706,45874,22858}));
+//        for (List<Integer> list: main.permuteUnique()) {
+        System.out.println(main.numSquarefulPerms(new int[]{1, 17, 8}));
+
+//            for (int num: list) System.out.print(num + ",");
+//            System.out.println();
+//        }
+        //13949,64411,26920,5204,2177,23617,44128,3455,47315,40706,45874,22858
     }
+
+    public boolean isCousins(TreeNode root, int x, int y) {
+        return isCousinsHelper(root, x, y) == 0;
+    }
+
+    private int isCousinsHelper(TreeNode root, int x, int y) {
+        if (root == null) return -1;
+        if (root.val == x || root.val == y) return 1;
+        int left = isCousinsHelper(root.left, x, y);
+        int right = isCousinsHelper(root.right, x, y);
+        if (left > 0 && right > 0) {
+            if (left == right) {
+                return (root.left.val == x && root.right.val == y) ||
+                        (root.right.val == x && root.left.val == y) ? -1 : 0;
+            } else return -1;
+        } else if (left >= 0) return left == 0 ? 0 : left + 1;
+        else if (right >= 0) return right == 0 ? 0 : right + 1;
+        else return -1;
+    }
+
+    public int orangesRotting(int[][] grid) {
+        if (grid == null || grid.length == 0) return 0;
+        Queue<int[]> queue = new LinkedList<>();
+        int[][] dirs = new int[][]{{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        int row = grid.length, col = grid[0].length;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (grid[i][j] == 2) queue.offer(new int[]{i, j});
+            }
+        }
+        int step = 3;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            while (size-- > 0) {
+                int[] poll = queue.poll();
+                for (int[] dir: dirs) {
+                    int x = poll[0] + dir[0], y = poll[1] + dir[1];
+                    if (x < 0 || x >= row || y < 0 || y >= col ||
+                    grid[x][y] != 1) continue;
+                    grid[x][y] = step;
+                    queue.offer(new int[]{x, y});
+                }
+            }
+            step++;
+        }
+        int min = 0;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (grid[i][j] == 1) return -1;
+                if (grid[i][j] >= 3) {
+                    min = Math.max(min, grid[i][j] - 2);
+                }
+            }
+        }
+        return min;
+    }
+
+//    public int minKBitFlips(int[] A, int K) {
+//
+//    }
+
+    public List<String> permutations(String set) {
+        List<String> res = new ArrayList<>();
+        if (set == null) return res;
+//        addPermutaions(set.toCharArray(), 0, res);
+        return res;
+    }
+
+    private int count = 0;
+    public int numSquarefulPerms(int[] A) {
+        Arrays.sort(A);
+        List<Integer> current = new ArrayList<>();
+        boolean[] visited = new boolean[A.length];
+        permute(current, A, visited);
+        return count;
+    }
+
+//    private void addPermutaions(int[] nums, int cur) {
+//        if (cur == nums.length) {
+//            for (int i = 0; i < nums.length - 1; i++) {
+//                int sum = nums[i] + nums[i + 1];
+//
+//            }
+//            count++;
+//            return;
+//        }
+//        Set<Integer> set = new HashSet<>();
+//        for (int i = cur; i < nums.length; i++) {
+//            if (set.add(nums[i])) {
+//                if ((cur == 0 || isPerfectSqaure(nums[cur - 1] + nums[i])) &&
+//                        (cur == nums.length - 1 || isPerfectSqaure(nums[cur + 1] + nums[i]))) {
+//                    swap(nums, i, cur);
+//                    addPermutaions(nums, cur + 1);
+//                    swap(nums, i, cur);
+//                };
+//            }
+//        }
+//    }
+//
+//    private void swap(int[] nums, int a, int b) {
+//        int c = nums[a];
+//        nums[a] = nums[b];
+//        nums[b] = c;
+//    }
+
+//    public List<List<Integer>> permuteUnique(int[] num) {
+//        Arrays.sort(num);
+//        List<List<Integer>> result = new ArrayList<>();
+//        List<Integer> current = new ArrayList<>();
+//        boolean[] visited = new boolean[num.length];
+//        permute(result, current, num, visited);
+//        return result;
+//    }
+
+    private void permute(List<Integer> current, int[] num, boolean[] visited) {
+        if (current.size() == num.length) {
+            count++;
+            return;
+        }
+        for (int i=0; i<visited.length; i++) {
+            if (!visited[i]) {
+                if (i > 0 && num[i] == num[i-1] && visited[i-1]) {
+                    return;
+                }
+                if (current.size() == 0 || isPerfectSqaure(current.get(current.size() - 1) + num[i])) {
+                    visited[i] = true;
+                    current.add(num[i]);
+                    permute(current, num, visited);
+                    current.remove(current.size()-1);
+                    visited[i] = false;
+                }
+            }
+        }
+    }
+
+    private boolean isPerfectSqaure(int num) {
+        double sqrt = Math.sqrt(num);
+        return sqrt - Math.floor(sqrt) == 0;
+    }
+
 }
 
 
---
+++ src/tree/bottom_up/Q993CousinsInBinaryTree.java
@@ -0,0 +1,80 @@
+package tree.bottom_up;
+
+import public_class.TreeNode;
+
+public class Q993CousinsInBinaryTree {
+
+    //Difficulty: Easy
+    //TAG: Tree
+    //TAG: bottom up
+
+    /**
+     * 993. Cousins in Binary Tree
+     * In a binary tree, the root node is at depth 0, and children of each depth k node are at depth k+1.
+     *
+     * Two nodes of a binary tree are cousins if they have the same depth, but have different parents.
+     *
+     * We are given the root of a binary tree with unique values, and the values x and y of two different nodes in the tree.
+     *
+     * Return true if and only if the nodes corresponding to the values x and y are cousins.
+     *
+     *
+     *
+     * Example 1:
+     *
+     *
+     * Input: root = [1,2,3,4], x = 4, y = 3
+     * Output: false
+     * Example 2:
+     *
+     *
+     * Input: root = [1,2,3,null,4,null,5], x = 5, y = 4
+     * Output: true
+     * Example 3:
+     *
+     *
+     *
+     * Input: root = [1,2,3,null,4], x = 2, y = 3
+     * Output: false
+     *
+     *
+     * Note:
+     *
+     * The number of nodes in the tree will be between 2 and 100.
+     * Each node has a unique integer value from 1 to 100.
+     */
+
+    /*
+    Solution:
+
+    Mark different value guide what we should do:
+    if -1 means, no x or y find from left or right tree or result is already false
+    if left and right both > 0 means we find both from left and right tree
+    check if they belongs to same node, if yes return -1 otherwise, we return 0 indicate result is true
+    else when any value is 0, we just return 0, otherwise return num + 1 if num > 0 (calculating height) or keep -1
+    finally only value 0 means true, num > 0 means we only find one x or y (one is in the subtree of another which will not be cousins)
+
+    Time: O(n)
+    Space: O(n)
+     */
+
+    public boolean isCousins(TreeNode root, int x, int y) {
+        return isCousinsHelper(root, x, y) == 0;
+    }
+
+    private int isCousinsHelper(TreeNode root, int x, int y) {
+        if (root == null) return -1;
+        if (root.val == x || root.val == y) return 1;
+        int left = isCousinsHelper(root.left, x, y);
+        int right = isCousinsHelper(root.right, x, y);
+        if (left > 0 && right > 0) {
+            if (left == right) {
+                return (root.left.val == x && root.right.val == y) ||
+                        (root.right.val == x && root.left.val == y) ? -1 : 0;
+            } else return -1;
+        } else if (left >= 0) return left == 0 ? 0 : left + 1;
+        else if (right >= 0) return right == 0 ? 0 : right + 1;
+        else return -1;
+    }
+
+}
