24c70d0fd1d006ec4f581536991aeb8cdcca57c6
StevenUpForever <j.chengzhi1989@gmail.com>
2019-02-17 23:42:50 -0800

    142. Linked List Cycle II

---
+++ src/linkedlist/Q142LinkedListCycleII.java
@@ -0,0 +1,71 @@
+package linkedlist;
+
+import public_class.ListNode;
+
+public class Q142LinkedListCycleII {
+
+    //Difficulty: Medium
+    //TAG: LinkedIn
+    //TAG: LinkedList
+
+    /**
+     * 142. Linked List Cycle II
+     * Given a linked list, return the node where the cycle begins. If there is no cycle, return null.
+     *
+     * To represent a cycle in the given linked list, we use an integer pos which represents the position (0-indexed) in the linked list where tail connects to. If pos is -1, then there is no cycle in the linked list.
+     *
+     * Note: Do not modify the linked list.
+     *
+     *
+     *
+     * Example 1:
+     *
+     * Input: head = [3,2,0,-4], pos = 1
+     * Output: tail connects to node index 1
+     * Explanation: There is a cycle in the linked list, where tail connects to the second node.
+     *
+     *
+     * Example 2:
+     *
+     * Input: head = [1,2], pos = 0
+     * Output: tail connects to node index 0
+     * Explanation: There is a cycle in the linked list, where tail connects to the first node.
+     *
+     *
+     * Example 3:
+     *
+     * Input: head = [1], pos = -1
+     * Output: no cycle
+     * Explanation: There is no cycle in the linked list.
+     *
+     *
+     *
+     *
+     * Follow up:
+     * Can you solve it without using extra space?
+     */
+
+    /*
+    Solution:
+    slow fast pointer find first met point, fast need start from head
+    then reset slow to first node, move slow/fast together by 1 each step, when met, the node is cycle start
+     */
+
+    public ListNode detectCycle(ListNode head) {
+        if (head == null) return null;
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) break;
+        }
+        if (fast == null || fast.next == null) return null;
+        slow = head;
+        while (slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return fast;
+    }
+
+}
