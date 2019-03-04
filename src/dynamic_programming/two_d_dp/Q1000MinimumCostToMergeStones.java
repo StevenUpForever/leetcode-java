3dfaaac34aa112cf6be9c9f0aaa80f1db458d76e
StevenUpForever <j.chengzhi1989@gmail.com>
2019-03-03 20:54:16 -0800

    1000. Minimum Cost to Merge Stones

--- src/dynamic_programming/two_d_dp/Q1000MinimumCostToMergeStones.java
+++ src/dynamic_programming/two_d_dp/Q1000MinimumCostToMergeStones.java
@@ -1,9 +1,10 @@
-package dynamic_programming.one_d_dp;
+package dynamic_programming.two_d_dp;
 
 public class Q1000MinimumCostToMergeStones {
 
     //Difficulty: hard
     //TAG: dp
+    //TODO: https://leetcode.com/problems/minimum-cost-to-merge-stones/discuss/247657/JAVA-Bottom-Up-%2B-Top-Down-DP-With-Explaination
 
     /**
      * 1000. Minimum Cost to Merge Stones
@@ -50,10 +51,49 @@ public class Q1000MinimumCostToMergeStones {
 
     /*
     Solution:
-
+    review: https://leetcode.com/problems/minimum-cost-to-merge-stones/discuss/247657/JAVA-Bottom-Up-%2B-Top-Down-DP-With-Explaination
      */
 
     public int mergeStones(int[] stones, int K) {
+        int len = stones.length;
+        if ((len - 1) % (K - 1) != 0) {
+            return -1;
+        }
 
+        int i, j, k, l, t;
+
+        int[] prefixSum = new int[len + 1];
+        for (i = 1; i <= len; i++) {
+            prefixSum[i] = prefixSum[i - 1] + stones[i - 1];
+        }
+
+        int max = 99999999;
+        int[][][] dp = new int[len + 1][len + 1][K + 1];
+        for (i = 1; i <= len; i++) {
+            for (j = 1; j <= len; j++) {
+                for (k = 1; k <= K; k++) {
+                    dp[i][j][k] = max;
+                }
+            }
+        }
+
+        for (i = 1; i <= len; i++) {
+            dp[i][i][1] = 0;
+        }
+
+        for (l = 2; l <= len; l++) {
+            for (i = 1; i <= len - l + 1; i++) {
+                j = i + l - 1;
+                for (k = 2; k <= K; k++) {
+                    for (t = i; t < j; t++) {
+                        dp[i][j][k] = Math.min(dp[i][j][k], dp[i][t][k - 1] + dp[t + 1][j][1]);
+                    }
+                }
+
+                dp[i][j][1] = dp[i][j][K] + prefixSum[j] - prefixSum[i - 1];
+            }
+        }
+
+        return dp[1][len][1];
     }
 }
