087d9ad8e42d27d56563b7180d725218717a9454
StevenUpForever <j.chengzhi1989@gmail.com>
2019-05-11 00:01:30 -0700

    567. Permutation in String

--- src/string/sliding_window/Q567PermutationInString.java
+++ src/string/sliding_window/Q567PermutationInString.java
@@ -36,7 +36,7 @@ public class Q567PermutationInString {
     /*
     Solution:
 
-    sliding window
+    sliding window filter by map
 
     Time: O(m + n)
     Space: O(m)
@@ -66,6 +66,41 @@ public class Q567PermutationInString {
             if (count == map.size()) return true;
         }
         return false;
+    }
+
+    /*
+    Solution 2:
+    sliding window, filter by int[26]
+
+    Time: O(m + n)
+    Space: O(1)
+     */
+
+    public boolean checkInclusion2(String s1, String s2) {
+        int len1 = s1.length(), len2 = s2.length();
+        if (len1 > len2) return false;
+
+        int[] count = new int[26];
+        for (int i = 0; i < len1; i++) {
+            count[s1.charAt(i) - 'a']++;
+            count[s2.charAt(i) - 'a']--;
+        }
+        if (allZero(count)) return true;
+
+        for (int i = len1; i < len2; i++) {
+            count[s2.charAt(i) - 'a']--;
+            count[s2.charAt(i - len1) - 'a']++;
+            if (allZero(count)) return true;
+        }
+
+        return false;
+    }
+
+    private boolean allZero(int[] count) {
+        for (int i = 0; i < 26; i++) {
+            if (count[i] != 0) return false;
+        }
+        return true;
     }
 
 }
