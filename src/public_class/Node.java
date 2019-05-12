15418e996eeb8d1c84df48c01e7e5db0730f3613
StevenUpForever <j.chengzhi1989@gmail.com>
2019-05-11 18:36:06 -0700

    add Node

---
+++ src/public_class/Node.java
@@ -0,0 +1,16 @@
+package public_class;
+
+// Definition for a Node.
+public class Node {
+    public int val;
+    public Node left;
+    public Node right;
+
+    public Node() {}
+
+    public Node(int _val,Node _left,Node _right) {
+        val = _val;
+        left = _left;
+        right = _right;
+    }
+};
