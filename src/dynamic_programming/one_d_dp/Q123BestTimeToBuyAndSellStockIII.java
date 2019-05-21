dbee280dcff4613564a5b48b2e761fe447d98833
StevenUpForever <j.chengzhi1989@gmail.com>
2019-05-20 18:41:37 -0700

    public int maxProfit(int[] prices) {

---
+++ src/dynamic_programming/one_d_dp/Q123BestTimeToBuyAndSellStockIII.java
@@ -0,0 +1,59 @@
+package dynamic_programming.one_d_dp;
+
+public class Q123BestTimeToBuyAndSellStockIII {
+
+    //Difficulty: hard
+    //TAG: dp
+    //TAG: array
+
+    /**
+     * 123. Best Time to Buy and Sell Stock III
+     * Say you have an array for which the ith element is the price of a given stock on day i.
+     *
+     * Design an algorithm to find the maximum profit. You may complete at most two transactions.
+     *
+     * Note: You may not engage in multiple transactions at the same time (i.e., you must sell the stock before you buy again).
+     *
+     * Example 1:
+     *
+     * Input: [3,3,5,0,0,3,1,4]
+     * Output: 6
+     * Explanation: Buy on day 4 (price = 0) and sell on day 6 (price = 3), profit = 3-0 = 3.
+     *              Then buy on day 7 (price = 1) and sell on day 8 (price = 4), profit = 4-1 = 3.
+     * Example 2:
+     *
+     * Input: [1,2,3,4,5]
+     * Output: 4
+     * Explanation: Buy on day 1 (price = 1) and sell on day 5 (price = 5), profit = 5-1 = 4.
+     *              Note that you cannot buy on day 1, buy on day 2 and sell them later, as you are
+     *              engaging multiple transactions at the same time. You must sell before buying again.
+     * Example 3:
+     *
+     * Input: [7,6,4,3,1]
+     * Output: 0
+     * Explanation: In this case, no transaction is done, i.e. max profit = 0.
+     */
+
+    /*
+    Solution:
+
+    Think about keep the profit and make 4 values indicate buy1, sell1, buy2, sell2
+
+    https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iii/discuss/39615/My-explanation-for-O(N)-solution!
+
+    Time: O(n)
+    Space: O(1)
+     */
+
+    public int maxProfit(int[] prices) {
+        int buy1 = Integer.MIN_VALUE, sell1 = 0, buy2 = Integer.MIN_VALUE, sell2 = 0;
+        for (int price: prices) {
+            buy1 = Math.max(buy1, -price);
+            sell1 = Math.max(sell1, price + buy1);
+            buy2 = Math.max(buy2, sell1 - price);
+            sell2 = Math.max(sell2, buy2 + price);
+        }
+        return sell2;
+    }
+
+}
