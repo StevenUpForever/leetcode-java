2c181d9108e56986ce587c94926938660323f1cf
Chengzhi Jia <j.chengzhi1989@gmail.com>
2020-09-03 23:19:50 -0700

    567. Permutation in String

--- src/string/sliding_window/Q567PermutationInString.java
+++ src/string/sliding_window/Q567PermutationInString.java
@@ -12,7 +12,8 @@ public class Q567PermutationInString {
 
     /**
      * 567. Permutation in String
-     * Given two strings s1 and s2, write a function to return true if s2 contains the permutation of s1. In other words, one of the first string's permutations is the substring of the second string.
+     * Given two strings s1 and s2, write a function to return true if s2 contains the permutation of s1.
+     * In other words, one of the first string's permutations is the substring of the second string.
      *
      *
      *
